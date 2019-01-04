285bf678a96be92b64926d2a8651af0fe4ac6e6a
blitheanon <blitheanon@qq.com>
2019-01-04 20:03:03 +0800

    Feature: 添加allFeeds对象测试套件

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,17 +3,20 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
+ // 这个文件为规格文件，Jasmine测试框架会读取规格文件包含的所有测试方法
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
+ //将所有测试套件放进了$()中，确保测试方法在Dom加载完毕后再执行
 $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    //第一个测试套件，只包含相关的测试方法，这个测试套件是关于阅读器测试的定义，allFeeds变量位于应用内
+    describe('订阅阅读器', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +24,8 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+         //第一个测试方法确保allFeeds变量被定义，且不为空
+        it('allFeeds定义', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,12 +35,28 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         //写一个测试方法，遍历allFeeds对象，确保其存在URL定义，且URL不为空
+         it('url已定义',function() {
+           for (feed of allFeeds) {
+             expect(feed.url).toBeDefined();
+             expect(feed.url).not.toBe('');
+           }
+         });
+
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         //写一个测试方法，遍历allFeeds对象，确保其name存在，且不为空
+         it('name已定义',function() {
+           for (feed of allFeeds) {
+             expect(feed.name).toBeDefined();
+             expect(feed.name).not.toBe('');
+           }
+         });
+
     });
 
 
--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ var allFeeds = [
     }
 ];
 
+
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
  * function when the API is loaded.
